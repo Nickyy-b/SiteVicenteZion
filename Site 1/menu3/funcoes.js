70e9362a11f4e889932a5c540bf04421955516a5
Nickyy-b <54724415+Nickyy-b@users.noreply.github.com>
2022-04-06 20:42:48 -0300

    Add files via upload

---
+++ Site 1/menu3/funcoes.js
@@ -0,0 +1,29 @@
+        // Função que abre e fecha os sub-menus
+        $(document).ready(function(){
+            // jquery or toggle sub menus
+            $('.sub-btn').click(function(){
+                $(this).next('.sub-menu').slideToggle(); 
+                $(this).next('.sub-sub-menu').slideToggle();
+                $(this).find('.dropdown').toggleClass('rotate'); // Rotatividade da seta do sub-menus
+            });
+            // jquery abri e fechar menu
+            $('.menu-btn').click(function(){
+                $('.side-bar').addClass('active');
+                $('.menu-btn').css("visibility", "hidden");
+            });
+
+            $('.close-btn').click(function(){
+                $('.side-bar').removeClass('active');
+                $('.menu-btn').css("visibility", "visible");
+            });
+        });
+
+        $('.slider-principal').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 4000
+          });
